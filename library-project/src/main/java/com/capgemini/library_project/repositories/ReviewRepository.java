d8de5c8f5525319bb15fef050882546b1b024603
ayushkale3456 <ayushkale35@gmail.com>
2025-05-13 22:01:22 +0530

    Added Repo, Services And Controller

--- library-project/src/main/java/com/capgemini/library_project/repositories/ReviewRepository.java
+++ library-project/src/main/java/com/capgemini/library_project/repositories/ReviewRepository.java
@@ -1,5 +1,9 @@
 package com.capgemini.library_project.repositories;
 
-public class ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capgemini.library_project.entities.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Long>{
 
 }
