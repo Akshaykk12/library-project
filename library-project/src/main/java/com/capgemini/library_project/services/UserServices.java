3245c1c1507a2fc45ec9e552acd7043a61304d1a
Aryan Parmanand <aparmanand22@gmail.com>
2025-05-14 09:26:39 +0530

    Commit for User fields..

--- library-project/src/main/java/com/capgemini/library_project/services/UserServices.java
+++ library-project/src/main/java/com/capgemini/library_project/services/UserServices.java
@@ -1,5 +1,18 @@
 package com.capgemini.library_project.services;
 
-public class UserServices {
+import java.util.List;
 
+import com.capgemini.library_project.entities.User;
+
+public interface UserServices {
+
+	List<User> getAllUsers();
+
+	User getUserById(Long userId);
+
+	User createUser(User user);
+
+	User updateUser(Long userId, User user);
+
+	boolean deleteUser(Long userId);
 }
