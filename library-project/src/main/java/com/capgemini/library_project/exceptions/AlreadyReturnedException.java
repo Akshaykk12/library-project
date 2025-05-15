e89bf907058210979c9b598276d634617e024f40
SANKET-023 <sanketmaral1205@gmail.com>
2025-05-15 20:52:49 +0530

    added custom exception classes .

---
+++ library-project/src/main/java/com/capgemini/library_project/exceptions/AlreadyReturnedException.java
@@ -0,0 +1,8 @@
+package com.capgemini.library_project.exceptions;
+
+public class AlreadyReturnedException  extends RuntimeException{
+	public AlreadyReturnedException(String message) {
+		super(message);
+	}
+
+}
---
+++ library-project/src/main/java/com/capgemini/library_project/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,9 @@
+package com.capgemini.library_project.exceptions;
+
+public class CategoryNotFoundException extends RuntimeException{
+	
+	public CategoryNotFoundException(String message) {
+		super(message);
+	}
+
+}
