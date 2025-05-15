2a0a106b131874bddbf6dfd15ae7666f91adcc5a
s4meersutar <sameersutar59@gmail.com>
2025-05-15 14:03:14 +0530

    added bookNotFoundException, borrowRecordNotFound, userAlreadyExistsException, userNotFoundException and globalExceptionHandler

---
+++ library-project/src/main/java/com/capgemini/library_project/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.capgemini.library_project.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(UserAlreadyExistsException.class)
+	public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<String> handleUserAlreadyExistsException(UserNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
+	@ExceptionHandler(BookNotFoundException.class)
+	public ResponseEntity<String> handleUserAlreadyExistsException(BookNotFoundException be) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(be.getMessage());
+	}
+	@ExceptionHandler(BorrowRecordNotFoundException.class)
+	public ResponseEntity<String> handleUserAlreadyExistsException(BorrowRecordNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+	}
+}
