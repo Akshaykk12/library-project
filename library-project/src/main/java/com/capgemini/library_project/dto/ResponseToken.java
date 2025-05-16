46f0eec3608ace038f27aa8994d876b1afff0e07
SANKET-023 <sanketmaral1205@gmail.com>
2025-05-16 10:27:33 +0530

    Added LoginDto and ResponseToken in Dto package.

---
+++ library-project/src/main/java/com/capgemini/library_project/dto/ResponseToken.java
@@ -0,0 +1,19 @@
+package com.capgemini.library_project.dto;
+
+public class ResponseToken {
+
+	String token;
+
+	public ResponseToken() {
+		super();
+	}
+
+	public ResponseToken(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+}
