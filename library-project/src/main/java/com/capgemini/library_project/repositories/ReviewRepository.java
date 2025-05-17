1f7b444631cdf2426c244e55f40d3495c68e57de
ayushkale3456 <ayushkale35@gmail.com>
2025-05-17 08:45:22 +0530

    Added Jwt jpql queries and did some changes in services and Controller

--- library-project/src/main/java/com/capgemini/library_project/repositories/ReviewRepository.java
+++ library-project/src/main/java/com/capgemini/library_project/repositories/ReviewRepository.java
@@ -1,9 +1,26 @@
 package com.capgemini.library_project.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 
 import com.capgemini.library_project.entities.Review;
 
-public interface ReviewRepository extends JpaRepository<Review, Long>{
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+	@Query("SELECT r FROM Review r WHERE r.book.bookId = :bookId")
+	List<Review> findByBookId(@Param("bookId") Long bookId);
+
+	@Query("SELECT r FROM Review r WHERE r.user.userId = :userId")
+	List<Review> findByUserId(@Param("userId") Long userId);
+
+	@Query("SELECT AVG(r.rating) FROM Review r WHERE r.book.bookId = :bookId")
+	Double findAverageRatingByBookId(@Param("bookId") Long bookId);
+
+	@Query("SELECT r FROM Review r WHERE r.rating >= :minRating")
+	List<Review> findReviewsWithMinRating(@Param("minRating") int minRating);
+
+	@Query("SELECT COUNT(r) FROM Review r WHERE r.book.bookId = :bookId")
+	Long countReviewsByBookId(@Param("bookId") Long bookId);
 }
